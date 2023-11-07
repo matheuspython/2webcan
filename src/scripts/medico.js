07b07a6c2de96a27233f16175b803ab341c93b22
matheus souza <64442354+matheuspython@users.noreply.github.com>
2023-11-07 15:18:26 -0300

    Add files via upload

---
+++ src/scripts/medico.js
@@ -0,0 +1,83 @@
+// Selecionar elementos HTML para a área do médico
+const videoElement = document.querySelectorAll(".video-element");
+const startRecordingButton = document.getElementById("start-recording");
+const stopRecordingButton = document.getElementById("stop-recording");
+const placeholder = document.getElementById("placeholder");
+const consultaForm = document.getElementById("consulta-form");
+const finalizarConsultaButton = document.getElementById("finalizar-consulta");
+
+// Variáveis para controle da gravação
+let mediaStream;
+let mediaRecorder;
+let recording = false; // Variável para controlar o estado da gravação
+
+// Função para iniciar a gravação da câmera
+startRecordingButton.addEventListener("click", async () => {
+    if (!recording) {
+        try {
+            mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
+
+            videoElement.forEach(video => {
+                video.srcObject = mediaStream;
+                video.style.display = "block";
+            });
+
+            mediaRecorder = new MediaRecorder(mediaStream);
+
+            mediaRecorder.ondataavailable = function (event) {
+                // Manipular os dados da gravação (por exemplo, salvar em um arquivo)
+            };
+
+            mediaRecorder.start();
+            startRecordingButton.disabled = true;
+            stopRecordingButton.disabled = false;
+            stopRecordingButton.classList.remove("hidden"); // Mostra o botão de parar a gravação
+
+            recording = true; // Atualiza o estado da gravação
+        } catch (error) {
+            alert("Erro ao acessar a webcam: " + error.message);
+        }
+    }
+});
+
+// Função para parar a gravação
+stopRecordingButton.addEventListener("click", () => {
+    if (recording) {
+        mediaRecorder.stop();
+        mediaStream.getTracks().forEach(track => track.stop());
+
+        videoElement.forEach(video => {
+            video.style.display = "none";
+        });
+
+        placeholder.style.display = "block";
+        startRecordingButton.disabled = false;
+        stopRecordingButton.disabled = true;
+        stopRecordingButton.classList.add("hidden"); // Esconde o botão de parar a gravação
+
+        recording = false; // Atualiza o estado da gravação
+    }
+});
+
+// Evento para mostrar o formulário da consulta
+finalizarConsultaButton.addEventListener("click", () => {
+    consultaForm.style.display = "none";
+    consultaDados.style.display = "block";
+
+    // Parar a gravação de vídeo
+    if (recording) {
+        mediaRecorder.stop();
+        mediaStream.getTracks().forEach(track => track.stop());
+
+        videoElement.forEach(video => {
+            video.style.display = "none";
+        });
+
+        placeholder.style.display = "block";
+        startRecordingButton.disabled = false;
+        stopRecordingButton.disabled = true;
+        stopRecordingButton.classList.add("hidden"); // Esconde o botão de parar a gravação
+
+        recording = false; // Atualiza o estado da gravação
+    }
+});
