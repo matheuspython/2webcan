6e975c60fcb95e6809ffb1ae8ca8d7eb7181bacf
matheus souza <64442354+matheuspython@users.noreply.github.com>
2023-11-07 15:04:01 -0300

    Add files via upload

--- src/scripts/cra.js
+++ src/scripts/cra.js
@@ -1,96 +1,58 @@
-
-// // Selecionar elementos HTML
-// const videoElement = document.querySelectorAll(".video-element");
-// // const placeholder = document.getElementById("placeholder");
-// const startRecordingButton = document.getElementById("start-recording");
-// const stopRecordingButton = document.getElementById("stop-recording");
-
-// // Variáveis para controle da gravação
-// let mediaStream;
-// let mediaRecorder;
-
-// // Função para iniciar a gravação da câmera
-// startRecordingButton.addEventListener("click", async () => {
-//     try {
-//         mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
-
-//         videoElement.forEach(e =>{
-//             videoElement.srcObject = mediaStream;
-//             videoElement.style.display = "block";
-//             // placeholder.style.display = "none";
-    
-//             mediaRecorder = new MediaRecorder(mediaStream);
-    
-//             mediaRecorder.ondataavailable = function (event) {
-//                 // Manipular os dados da gravação (por exemplo, salvar em um arquivo)
-//             };
-    
-//             mediaRecorder.start();
-//             startRecordingButton.disabled = true;
-//             stopRecordingButton.disabled = false;
-
-//         })
-
-//     } catch (error) {
-//         alert("Erro ao acessar a webcam: " + error.message);
-//     }
-// });
-
-// // Função para parar a gravação
-// stopRecordingButton.addEventListener("click", () => {
-//     mediaRecorder.stop();
-//     mediaStream.getTracks().forEach(track => track.stop());
-//     videoElement.style.display = "none";
-//     placeholder.style.display = "block";
-//     startRecordingButton.disabled = false;
-//     stopRecordingButton.disabled = true;
-// });
- 
 // Selecionar elementos HTML
 const videoElement = document.querySelectorAll(".video-element");
 const startRecordingButton = document.getElementById("start-recording");
 const stopRecordingButton = document.getElementById("stop-recording");
-const placeholder = document.getElementById("placeholder"); // Adicione esta linha para selecionar o elemento com id "placeholder"
+const placeholder = document.getElementById("placeholder");
 
 // Variáveis para controle da gravação
 let mediaStream;
 let mediaRecorder;
+let recording = false; // Variável para controlar o estado da gravação
 
 // Função para iniciar a gravação da câmera
 startRecordingButton.addEventListener("click", async () => {
-    try {
-        mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
+    if (!recording) {
+        try {
+            mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
 
-        videoElement.forEach(video => {
-            video.srcObject = mediaStream;
-            video.style.display = "block";
-        });
+            videoElement.forEach(video => {
+                video.srcObject = mediaStream;
+                video.style.display = "block";
+            });
 
-        mediaRecorder = new MediaRecorder(mediaStream);
+            mediaRecorder = new MediaRecorder(mediaStream);
 
-        mediaRecorder.ondataavailable = function (event) {
-            // Manipular os dados da gravação (por exemplo, salvar em um arquivo)
-        };
+            mediaRecorder.ondataavailable = function (event) {
+                // Manipular os dados da gravação (por exemplo, salvar em um arquivo)
+            };
 
-        mediaRecorder.start();
-        startRecordingButton.disabled = true;
-        stopRecordingButton.disabled = false;
+            mediaRecorder.start();
+            startRecordingButton.disabled = true;
+            stopRecordingButton.disabled = false;
+            stopRecordingButton.classList.remove("hidden"); // Mostra o botão de parar a gravação
 
-    } catch (error) {
-        alert("Erro ao acessar a webcam: " + error.message);
+            recording = true; // Atualiza o estado da gravação
+        } catch (error) {
+            alert("Erro ao acessar a webcam: " + error.message);
+        }
     }
 });
 
 // Função para parar a gravação
 stopRecordingButton.addEventListener("click", () => {
-    mediaRecorder.stop();
-    mediaStream.getTracks().forEach(track => track.stop());
+    if (recording) {
+        mediaRecorder.stop();
+        mediaStream.getTracks().forEach(track => track.stop());
 
-    videoElement.forEach(video => {
-        video.style.display = "none";
-    });
+        videoElement.forEach(video => {
+            video.style.display = "none";
+        });
 
-    placeholder.style.display = "block"; // Certifique-se de que 'placeholder' esteja definido
-    startRecordingButton.disabled = false;
-    stopRecordingButton.disabled = true;
+        placeholder.style.display = "block";
+        startRecordingButton.disabled = false;
+        stopRecordingButton.disabled = true;
+        stopRecordingButton.classList.add("hidden"); // Esconde o botão de parar a gravação
+
+        recording = false; // Atualiza o estado da gravação
+    }
 });
