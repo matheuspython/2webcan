5fefd74bcfaff4e4da8a7b79c93866375742aed9
matheus souza <64442354+matheuspython@users.noreply.github.com>
2023-11-07 15:54:30 -0300

    Add files via upload

--- src/scripts/medico.js
+++ src/scripts/medico.js
@@ -1,84 +1,81 @@
-// Selecionar elementos HTML
-const videoElement = document.querySelectorAll(".video-element");
-const startRecordingButton = document.getElementById("start-recording");
-const stopRecordingButton = document.getElementById("stop-recording");
-const placeholder = document.getElementById("placeholder");
-const consultaForm = document.getElementById("consulta-form");
-const finalizarConsultaButton = document.getElementById("finalizar-consulta");
-const consultaDados = document.getElementById("consulta-dados");
+document.addEventListener("DOMContentLoaded", function () {
+    // Selecionar elementos HTML
+    const videoElement = document.querySelectorAll(".video-element");
+    const startRecordingButton = document.getElementById("start-recording");
+    const placeholder = document.getElementById("placeholder");
+    const consultaForm = document.getElementById("consulta-form");
+    const finalizarConsultaButton = document.getElementById("finalizar-consulta");
+    const consultaDados = document.getElementById("consulta-dados");
+    const cpfDados = document.getElementById("cpf-dados");
+    const hospitalDados = document.getElementById("hospital-dados");
+    const situacaoDados = document.getElementById("situacao-dados");
+    const dataDados = document.getElementById("data-dados");
 
-// Variáveis para controle da gravação
-let mediaStream;
-let mediaRecorder;
-let recording = false; // Variável para controlar o estado da gravação
+    // Variáveis para controle da gravação
+    let mediaStream;
+    let mediaRecorder;
+    let recording = false; // Variável para controlar o estado da gravação
 
-// Função para iniciar a gravação da câmera
-startRecordingButton.addEventListener("click", async () => {
-    if (!recording) {
-        try {
-            mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
+    // Função para iniciar a gravação da câmera
+    startRecordingButton.addEventListener("click", async () => {
+        if (!recording) {
+            try {
+                mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
 
-            videoElement.forEach(video => {
-                video.srcObject = mediaStream;
-                video.style.display = "block";
-            });
+                videoElement.forEach(video => {
+                    video.srcObject = mediaStream;
+                    video.style.display = "block";
+                });
 
-            mediaRecorder = new MediaRecorder(mediaStream);
+                mediaRecorder = new MediaRecorder(mediaStream);
 
-            mediaRecorder.ondataavailable = function (event) {
-                // Manipular os dados da gravação (por exemplo, salvar em um arquivo)
-            };
+                mediaRecorder.ondataavailable = function (event) {
+                    // Manipular os dados da gravação (por exemplo, salvar em um arquivo)
+                };
 
-            mediaRecorder.start();
-            startRecordingButton.disabled = true;
-            stopRecordingButton.disabled = false;
-            stopRecordingButton.classList.remove("hidden"); // Mostra o botão de parar a gravação
+                mediaRecorder.start();
+                startRecordingButton.disabled = true;
 
-            recording = true; // Atualiza o estado da gravação
-        } catch (error) {
-            alert("Erro ao acessar a webcam: " + error.message);
+                recording = true; // Atualiza o estado da gravação
+            } catch (error) {
+                alert("Erro ao acessar a webcam: " + error.message);
+            }
         }
-    }
-});
+    });
 
-// Função para parar a gravação
-stopRecordingButton.addEventListener("click", () => {
-    if (recording) {
-        mediaRecorder.stop();
-        mediaStream.getTracks().forEach(track => track.stop());
+    // Função para parar a gravação
+    // Para parar a gravação, você pode adicionar um evento em um elemento específico, se necessário.
+    // Se você desejar que a gravação seja interrompida quando o formulário é enviado, isso pode ser feito lá.
 
-        videoElement.forEach(video => {
-            video.style.display = "none";
-        });
+    // Evento para mostrar o formulário da consulta
+    finalizarConsultaButton.addEventListener("click", () => {
+        consultaForm.style.display = "none";
+        consultaDados.style.display = "block";
 
-        placeholder.style.display = "block";
-        startRecordingButton.disabled = false;
-        stopRecordingButton.disabled = true;
-        stopRecordingButton.classList.add("hidden"); // Esconde o botão de parar a gravação
+        // Parar a gravação de vídeo (se necessário) pode ser feito aqui.
+    });
 
-        recording = false; // Atualiza o estado da gravação
-    }
-});
+    // Adicionar o código para exibir os dados do formulário na tela após o envio
+    consultaForm.addEventListener("submit", function (event) {
+        event.preventDefault(); // Impede o envio padrão do formulário
 
-// Evento para mostrar o formulário da consulta
-finalizarConsultaButton.addEventListener("click", () => {
-    consultaForm.style.display = "none";
-    consultaDados.style.display = "block";
+        // Recupere os valores dos campos do formulário
+        const cpfValue = document.getElementById("cpf").value;
+        const hospitalValue = document.getElementById("hospital").value;
+        const situacaoValue = document.getElementById("situacao").value;
+        const dataValue = document.getElementById("data").value;
+        const leftSide = document.querySelector('.left-side')
+        const RightSide = document.querySelector('.right-side')
+        leftSide.classList.add("hidden")
+        RightSide.classList.add('large')
 
-    // Parar a gravação de vídeo
-    if (recording) {
-        mediaRecorder.stop();
-        mediaStream.getTracks().forEach(track => track.stop());
+        // Exiba os valores na tela
+        cpfDados.textContent = `CPF do Paciente: ${cpfValue}`;
+        hospitalDados.textContent = `Hospital: ${hospitalValue}`;
+        situacaoDados.textContent = `Situação do Paciente: ${situacaoValue}`;
+        dataDados.textContent = `Data da Consulta: ${dataValue}`;
 
-        videoElement.forEach(video => {
-            video.style.display = "none";
-        });
-
-        placeholder.style.display = "block";
-        startRecordingButton.disabled = false;
-        stopRecordingButton.disabled = true;
-        stopRecordingButton.classList.add("hidden"); // Esconde o botão de parar a gravação
-
-        recording = false; // Atualiza o estado da gravação
-    }
+        consultaForm.style.display = "none";
+        consultaDados.style.display = "block";
+    });
 });
