a935dc61631cf82053bbd61edd9a8e00faa50b27
matheus souza <64442354+matheuspython@users.noreply.github.com>
2023-11-14 19:16:13 -0300

    updates

--- src/scripts/cadastro.js
+++ src/scripts/cadastro.js
@@ -1,52 +1,8 @@
 const form = document.getElementById('cadastro-form');
-function validarDigitoVerificadorRG(rg) {
-// Remova qualquer caractere não numérico do RG
-rg = rg.replace(/[^\d]+/g, '');
-
-// Verifique se o RG tem um formato válido com 9 dígitos
-if (rg.length !== 9) {
-return false;
-}
-
-// Separe os dígitos do RG em um array
-const digitos = rg.split('');
-
-// Defina os pesos para cálculo dos dígitos
-const pesos = [2, 3, 4, 5, 6, 7, 8, 9];
-
-// Inicialize o total para cálculo
-let total = 0;
-
-// Calcule o total multiplicando cada dígito pelo peso correspondente
-for (let i = 0; i < 8; i++) {
-total += parseInt(digitos[i]) * pesos[i];
-}
-
-// Calcule o resto da divisão do total por 11
-const resto = total % 11;
-
-// Verifique se o dígito verificador é válido
-let digitoVerificador = 11 - resto;
-if (digitoVerificador === 10) {
-digitoVerificador = 'X'; // Caso especial: dígito verificador X
-} else if (digitoVerificador === 11) {
-digitoVerificador = '0'; // Caso especial: dígito verificador 0
-}
-
-// Verifique se o dígito verificador calculado corresponde ao último dígito do RG
-return digitoVerificador === digitos[8];
-}
-
-// Exemplo de uso:
-const rg = "39.406.714-9"; // Substitua pelo RG que deseja validar
-if (validarDigitoVerificadorRG(rg)) {
-console.log("Dígito verificador válido");
-} else {
-console.log("Dígito verificador inválido");
-}
 
 function validarCPF(cpf) {
-    cpf = cpf.replace(/[^\d]+/g, ''); // Remove caracteres não numéricos
+    // Remove caracteres não numéricos
+    cpf = cpf.replace(/[^\d]+/g, '');
 
     if (cpf == '' || cpf.length != 11) {
         return false;
@@ -112,10 +68,11 @@ form.addEventListener('submit', function (e) {
         document.getElementById('cpf').style.border = '1px solid red';
     }
 
-    if (!validarDigitoVerificadorRG(rg)) {
-        errors.push('RG inválido.');
-        document.getElementById('rg').style.border = '1px solid red';
-    }
+    // Removido o trecho de validação do RG
+    // if (!validarDigitoVerificadorRG(rg)) {
+    //     errors.push('RG inválido.');
+    //     document.getElementById('rg').style.border = '1px solid red';
+    // }
 
     if (!endereco) {
         errors.push('Endereço é obrigatório.');
